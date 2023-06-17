cc04c461d3b6dd6f92a1da9f0e8fd237b3373e13
Yulia Glagola <glagolajulia@gmail.com>
2023-06-17 21:13:31 +0400

    starting html and installing dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import style from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <aside>
+        <ul>
+          <li>
+            <div>Step 1</div>
+            <div>Your info</div>
+          </li>
+          <li>
+            <div>Step 2</div>
+            <div>Select plan</div>
+          </li>
+          <li>
+            <div>Step 3</div>
+            <div>Add-ons</div>
+          </li>
+          <li>
+            <div>Step 4</div>
+            <div>Summary</div>
+          </li>
+        </ul>
+      </aside>
+      <main>
+        <h1>Multi step form</h1>
+        <form action="">
+          <h2>Personal info</h2>
+          <div>Please provide your name, email address, and phone number.</div>
+          <label htmlFor="">
+            Name
+            <input type="text" />
+          </label>
+          <label htmlFor="">
+            Email Address
+            <input type="text" />
+          </label>
+          <label htmlFor="">
+            Phone Number
+            <input type="text" />
+          </label>
+          <button>Next Step</button>
+        </form>
+      </main>
     </div>
   );
 }
