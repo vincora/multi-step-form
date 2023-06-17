6ece36bca28ad9e697c2f81c05ab4c64b424024b
Yulia Glagola <glagolajulia@gmail.com>
2023-06-17 21:25:33 +0400

    form first step html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import style from "./App.module.scss";
+import { useForm } from "react-hook-form";
 
 function App() {
+
+  const {register, handleSubmit} = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
     <div>
       <aside>
@@ -24,21 +29,21 @@ function App() {
         </ul>
       </aside>
       <main>
-        <h1>Multi step form</h1>
-        <form action="">
+        <h1 hidden>Multi step form</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <h2>Personal info</h2>
           <div>Please provide your name, email address, and phone number.</div>
           <label htmlFor="">
             Name
-            <input type="text" />
+            <input type="text" {...register('name', {required: 'This field is required'})} placeholder="e.g. Stephen King"/>
           </label>
           <label htmlFor="">
             Email Address
-            <input type="text" />
+            <input type="text" {...register('email', {required: 'This field is required'})} placeholder="e.g. stephenking@lorem.com"/>
           </label>
           <label htmlFor="">
             Phone Number
-            <input type="text" />
+            <input type="text" {...register('phone', {required: 'This field is required'})} placeholder="e.g. +1 234 567 890"/>
           </label>
           <button>Next Step</button>
         </form>
