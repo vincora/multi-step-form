77dd053fcf217eebe92a605890e3b4b27881783c
Yulia Glagola <glagolajulia@gmail.com>
2023-06-19 20:41:08 +0400

    sidebar js refactoring

--- src/App.js
+++ src/App.js
@@ -3,31 +3,61 @@ import { useForm } from "react-hook-form";
 import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-
-  const {register, handleSubmit} = useForm();
-  const onSubmit = data => console.log(data);
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   return (
     <div className={style.layoutWrapper}>
       <div className={style.layout}>
-        <Sidebar/>
-        <main>
+        <div className={style.layout__sidebar}>
+          <Sidebar />
+        </div>
+        <main className={style.layout__main}>
           <h1 hidden>Multi step form</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <h2>Personal info</h2>
-            <div>Please provide your name, email address, and phone number.</div>
-            <label htmlFor="">
-              Name
-              <input type="text" {...register('name', {required: 'This field is required'})} placeholder="e.g. Stephen King"/>
-            </label>
-            <label htmlFor="">
-              Email Address
-              <input type="text" {...register('email', {required: 'This field is required'})} placeholder="e.g. stephenking@lorem.com"/>
-            </label>
-            <label htmlFor="">
-              Phone Number
-              <input type="text" {...register('phone', {required: 'This field is required'})} placeholder="e.g. +1 234 567 890"/>
-            </label>
+            <h2 className={style.title}>Personal info</h2>
+            <div className={style.subtitle}>
+              Please provide your name, email address, and phone number.
+            </div>
+            <ul className={style.list}>
+              <li className={style.listItem}>
+                <label htmlFor="">
+                  Name
+                </label>
+                <input
+                    type="text"
+                    {...register("name", {
+                      required: "This field is required",
+                    })}
+                    placeholder="e.g. Stephen King"
+                  />
+              </li>
+              <li>
+                <label htmlFor="">
+                  Email Address
+                </label>
+                <input
+                    type="text"
+                    {...register("email", {
+                      required: "This field is required",
+                    })}
+                    placeholder="e.g. stephenking@lorem.com"
+                  />
+              </li>
+              <li>
+                <label htmlFor="">
+                  Phone Number
+                </label>
+                <input
+                    type="text"
+                    {...register("phone", {
+                      required: "This field is required",
+                    })}
+                    placeholder="e.g. +1 234 567 890"
+                  />
+              </li>
+            </ul>
+
             <button>Next Step</button>
           </form>
         </main>
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,38 +1,23 @@
 import React from "react";
-import style from './Sidebar.module.scss';
+import style from "./Sidebar.module.scss";
 
 const Sidebar = () => {
+  const titles = ["Your info", "Select plan", "Add-ons", "Summary"];
+
   return (
     <aside className={style.sidebar}>
       <ul>
-        <li className={style.item}>
-          <div className={style.itemNumber}>1</div>
-          <div className={style.itemInfo}>
-              <div className={style.itemSubTitle}>Step 1</div>
-              <div className={style.itemTitle}>Your info</div>
-          </div>
-        </li>
-        <li className={style.item}>
-          <div className={style.itemNumber}>2</div>
-          <div className={style.itemInfo}>
-              <div className={style.itemSubTitle}>Step 2</div>
-              <div className={style.itemTitle}>Select plan</div>
-          </div>
-        </li>
-        <li className={style.item}>
-          <div className={style.itemNumber}>3</div>
-          <div className={style.itemInfo}>
-              <div className={style.itemSubTitle}>Step 3</div>
-              <div className={style.itemTitle}>Add-ons</div>
-          </div>
-        </li>
-        <li className={style.item}>
-          <div className={style.itemNumber}>4</div>
-          <div className={style.itemInfo}>
-              <div className={style.itemSubTitle}>Step 4</div>
-              <div className={style.itemTitle}>Summary</div>
-          </div>
-        </li>
+        {titles.map((item, number) => {
+          return (
+            <li className={style.item}>
+              <div className={style.itemNumber}>{number+1}</div>
+              <div className={style.itemInfo}>
+                <div className={style.itemSubTitle}>Step {number+1}</div>
+                <div className={style.itemTitle}>{item}</div>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
