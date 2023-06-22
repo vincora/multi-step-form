bbd5528312b3ea61f4198500384e890411fee708
Yulia Glagola <julichis@mail.ru>
2023-06-22 19:52:51 +0400

    included inputs inside step1 component

--- src/components/step1/Step1.js
+++ src/components/step1/Step1.js
@@ -2,28 +2,38 @@ import React from "react";
 import style from "./Step1.module.scss";
 
 const Step1 = () => {
-  const inputs = [
-    { label: "Name", placeholder: "e.g. Stephen King" },
-    { label: "Email Address", placeholder: "e.g. stephenking@lorem.com" },
-    { label: "Phone Number", placeholder: "e.g. +1 234 567 890" },
-  ];
-  const Input = ({ label, placeholder }) => {
-    return (
-      <label>
-        {label}
-        <input type="text" placeholder={placeholder} className={style.input} />
-      </label>
-    );
-  };
   return (
     <ul className={style.list}>
-      {inputs.map(({ label, placeholder }) => {
-        return (
-          <li className={style.list__item} key={label}>
-            <Input label={label} placeholder={placeholder} />
-          </li>
-        );
-      })}
+      <li className={style.list__item}>
+        <label>
+          Name
+          <input
+            type="text"
+            placeholder="e.g. Stephen King"
+            className={style.input}
+          />
+        </label>
+      </li>
+      <li className={style.list__item}>
+        <label>
+          Email Address
+          <input
+            type="text"
+            placeholder="e.g. stephenking@lorem.com"
+            className={style.input}
+          />
+        </label>
+      </li>
+      <li className={style.list__item}>
+        <label>
+          Phone Number
+          <input
+            type="text"
+            placeholder="e.g. +1 234 567 890"
+            className={style.input}
+          />
+        </label>
+      </li>
     </ul>
   );
 };
