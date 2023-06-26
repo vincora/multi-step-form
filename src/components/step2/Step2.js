e7405b9c965c239a6294968a57a8c417d7d67714
Yulia Glagola <julichis@mail.ru>
2023-06-26 21:47:22 +0400

    step 2 toggle

--- src/components/step2/Step2.js
+++ src/components/step2/Step2.js
@@ -1,35 +1,63 @@
-import React from 'react';
-import style from './Step2.module.scss';
+import { useState } from "react";
+import style from "./Step2.module.scss";
+import arcadeIcon from "../../images/icon-arcade.svg";
+import advancedIcon from "../../images/icon-advanced.svg";
+import proIcon from "../../images/icon-pro.svg";
 
 const Step2 = () => {
-  return (
-    <ul className={style.plansList}>
-        <li className={style.plan}>
-            <div className={style.plan__img}></div>
-            <div className={style.plan__description}>
-                <div className={style.plan__title}>Arcade</div>
-                <div className={style.plan__price}>$9/mo</div>
-                <div className={style.plan__discount}>2 months free</div>
-            </div>
-        </li>
-        <li className={style.plan}>
-            <div className={style.plan__img}></div>
-            <div className={style.plan__description}>
-                <div className={style.plan__title}>Advanced</div>
-                <div className={style.plan__price}>$12/mo</div>
-                <div className={style.plan__discount}>2 months free</div>
-            </div>
-        </li>
-        <li className={style.plan}>
-            <div className={style.plan__img}></div>
-            <div className={style.plan__description}>
-                <div className={style.plan__title}>Pro</div>
-                <div className={style.plan__price}>$15/mo</div>
-                <div className={style.plan__discount}>2 months free</div>
-            </div>
-        </li>
-    </ul>
-  )
-}
+  const [term, setTerm] = useState("monthly");
 
-export default Step2
+  return (
+    <div>
+      <ul className={style.plansList}>
+        <li className={style.plan}>
+          <div className={style.plan__img}>
+            <img src={arcadeIcon} alt="" />
+          </div>
+          <div className={style.plan__description}>
+            <div className={style.plan__title}>Arcade</div>
+            <div className={style.plan__price}>$9/mo</div>
+            <div className={style.plan__discount}>2 months free</div>
+          </div>
+        </li>
+        <li className={style.plan}>
+          <div className={style.plan__img}>
+            <img src={advancedIcon} alt="" />
+          </div>
+          <div className={style.plan__description}>
+            <div className={style.plan__title}>Advanced</div>
+            <div className={style.plan__price}>$12/mo</div>
+            <div className={style.plan__discount}>2 months free</div>
+          </div>
+        </li>
+        <li className={style.plan}>
+          <div className={style.plan__img}>
+            <img src={proIcon} alt="" />
+          </div>
+          <div className={style.plan__description}>
+            <div className={style.plan__title}>Pro</div>
+            <div className={style.plan__price}>$15/mo</div>
+            <div className={style.plan__discount}>2 months free</div>
+          </div>
+        </li>
+      </ul>
+      <div className={style.term}>
+        <div>
+          Monthly
+        </div>
+        <label className={style.term__switch}>
+          <input
+            type="checkbox"
+            className={style.term__checkbox}
+          />
+          <span className={style.term__slider}></span>
+        </label>
+        <div>
+          Yearly
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Step2;
