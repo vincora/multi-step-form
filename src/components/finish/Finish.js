38f41c1be5972e92c33bff166b770cb1ef1215fc
Yulia Glagola <julichis@mail.ru>
2023-07-11 22:22:48 +0400

    moved icon to finish component folder

--- src/components/finish/Finish.js
+++ src/components/finish/Finish.js
@@ -1,16 +1,18 @@
-import style from './Finish.module.scss';
-import finishIcon from '../../images/icon-thank-you.svg';
+import style from "./Finish.module.scss";
+import finishIcon from "./images/icon-thank-you.svg";
 
-const Finish = ({title, description}) => {
+const Finish = ({ title, description }) => {
   return (
     <div className={style.finish}>
-        <div className={style.finish__body}>
-            <div className={style.finish__icon}><img src={finishIcon} alt="Thank you!" /></div>
-            <div className={style.finish__title}>{title}</div>
-            <div className={style.finish__description}>{description}</div>
+      <div className={style.finish__body}>
+        <div className={style.finish__icon}>
+          <img src={finishIcon} alt="Thank you!" />
         </div>
+        <div className={style.finish__title}>{title}</div>
+        <div className={style.finish__description}>{description}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Finish
+export default Finish;
