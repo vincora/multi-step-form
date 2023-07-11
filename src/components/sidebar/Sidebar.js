75204353a9f14dd8554b9958a84dba96edce13f8
Yulia Glagola <julichis@mail.ru>
2023-07-11 22:23:31 +0400

    fixed naming for map index

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,18 +2,24 @@ import React from "react";
 import style from "./Sidebar.module.scss";
 import cn from "classnames";
 
-const Sidebar = ({step}) => {
+const Sidebar = ({ step }) => {
   const titles = ["Your info", "Select plan", "Add-ons", "Summary"];
 
   return (
     <aside className={style.sidebar}>
       <ul>
-        {titles.map((item, number) => {
+        {titles.map((item, index) => {
           return (
             <li className={style.item} key={item}>
-              <div className={cn(style.item__number, {[style.item__number_active]: step === number+1})}>{number+1}</div>
+              <div
+                className={cn(style.item__number, {
+                  [style.item__number_active]: step === index + 1,
+                })}
+              >
+                {index + 1}
+              </div>
               <div className={style.item__info}>
-                <div className={style.item__subtitle}>Step {number+1}</div>
+                <div className={style.item__subtitle}>Step {index + 1}</div>
                 <div className={style.item__title}>{item}</div>
               </div>
             </li>
